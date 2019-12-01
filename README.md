2059e92bcea6a476c9be2f345308608f77e4feb7
Indre <hic.3t.nunc@gmail.com>
2019-12-01 11:03:50 +0100

    Update readme with instructions

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Live test plugin
 Chrome extension
+
+## Running it
+
+1. Fork or clone the repo.
+2. Open Chrome, go to `chrome://extensions/`
+3. Click **load unpacked** and choose the `src` folder of this repository (make sure `manifest.json` is in it!)
+4. The extension should appear in your extension list. Make sure it's enabled!
+
+Go to some page, open inspector and look at the console. Click on some element and it should output its selector. To check if it's the correct selector, you can call `document.querySelectorAll('GENERATED SELECTOR')` and check if it returns only one element.
--- src/content.js
+++ src/content.js
@@ -6,15 +6,17 @@ window.addEventListener('click', function (event) {
   event.stopPropagation()
   let clickedElement = event.target
   while (clickedElement.parentNode !== null) {
+    // IDs are used on a single element, so no need to go anywhere further
     if (clickedElement.id) {
       selector.unshift('#' + clickedElement.id)
       break
     } else {
       elTagName = clickedElement.tagName.toLowerCase()
       let children = clickedElement.parentNode.children
+      // Checks if current element has siblings
       if (children !== null) {
         let repeats = false
-        // check if any previous elements repeat
+        // checks if any previous elements repeat (if we need to generate 'nth-child')
         for (let j = 0; j < children.length; j++) {
           if (clickedElement === children[j]) {
             continue
@@ -26,6 +28,7 @@ window.addEventListener('click', function (event) {
         if (!repeats) {
           selector.unshift(elTagName)
         } else {
+          // Counts which child it is
           while (clickedElement.previousElementSibling) {
             clickedElement = clickedElement.previousElementSibling
             i++
