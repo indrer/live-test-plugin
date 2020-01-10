dc495024cea1f00c56dad9e993ffd77bb38885c1
Indre <hic.3t.nunc@gmail.com>
2020-01-10 18:24:36 +0100

    Ignore newline in string escape

--- src/js/util/stringEscape.js
+++ src/js/util/stringEscape.js
@@ -1,6 +1,6 @@
 // Method used to escape strings in Java rules
 export let escapeString = function (str) {
   const escape = require('safe-string-literal').escape
-  let exclude = ['`', '\'', '\t']
+  let exclude = ['`', '\'', '\t', '\n', '\r']
   return escape(str, exclude)
 }
